f3ca50b9dabf22c997e1de131d20395fd11ee513
손민석 <zonins3@gmail.com>
2026-03-06 16:38:17 +0900

    섹션 7. 동물 앨범 만들기-2-1

---
+++ section7/album-project/src/components/api.js
@@ -0,0 +1,13 @@
+const API_URL = 'https://animal-api-two.vercel.app/';
+
+export const getData = async () => {
+  const res = await fetch(API_URL);
+  try {
+    if (res) {
+      const data = await res.json();
+      return data.photos;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};

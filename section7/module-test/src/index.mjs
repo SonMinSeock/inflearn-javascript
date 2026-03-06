18c200d02f965ca58020971e858ff4675214cac4
손민석 <zonins3@gmail.com>
2026-03-06 16:18:48 +0900

    섹션 7. 모듈 시스템이란

---
+++ section7/module-test/src/index.mjs
@@ -0,0 +1,8 @@
+// import { num, add, User } from './test.mjs';
+// import * as testModule from './test.mjs';
+import User from './test.mjs';
+
+// console.log(testModule.num);
+// console.log(testModule.add(10, 20));
+// console.log(new testModule.User('Son'));
+console.log(new User('Son'));

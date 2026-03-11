497ce41a5da34f2920a6d983c2ef51e2e4eb2cba
손민석 <zonins3@gmail.com>
2026-03-11 12:42:54 +0900

    섹션 9. 컴포넌트화 작업

---
+++ section9/src/components/CityList.js
@@ -0,0 +1,17 @@
+export default function CityList() {
+  this.state = {};
+
+  this.$target = document.createElement('div');
+  this.$target.className = 'header';
+
+  this.template = () => {};
+
+  this.render = () => {};
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
+}
---
+++ section9/src/components/RegionList.js
@@ -0,0 +1,17 @@
+export default function RegionList() {
+  this.state = {};
+
+  this.$target = document.createElement('div');
+  this.$target.className = 'header';
+
+  this.template = () => {};
+
+  this.render = () => {};
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
+}
